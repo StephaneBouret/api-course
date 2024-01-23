e1124611aa8cb48dee986381b9ad6ff00592db47
Stéphane BOURET <sbouret@orange.fr>
2024-01-23 09:41:13 +0100

    Mise en place de la structure

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use DateTimeImmutable;
+use App\Entity\Invoice;
+use App\Entity\Customer;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    protected $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+
+        for ($u = 0; $u < 10; $u++) {
+            $user = new User;
+            $chrono = 1;
+            $hash = $this->passwordHasher->hashPassword($user, "password");
+            $user->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setEmail($faker->email)
+                ->setPassword($hash);
+
+            $manager->persist($user);
+            
+            for ($c = 0; $c < 30; $c++) {
+                $customer = new Customer;
+                $customer->setFirstName($faker->firstName())
+                    ->setLastName($faker->lastName)
+                    ->setCompany($faker->company)
+                    ->setEmail($faker->email)
+                    ->setUser($user);
+    
+                $manager->persist($customer);
+    
+                for ($i = 0; $i < mt_rand(3, 10); $i++) {
+                    $invoice = new Invoice;
+                    $invoice->setAmount($faker->price(4000, 20000))
+                        ->setSendAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-6 months')))
+                        ->setStatus($faker->randomElement(['SEND', 'PAID', 'CANCELLED']))
+                        ->setCustomer($customer)
+                        ->setChrono($chrono);
+    
+                    $chrono++;
+    
+                    $manager->persist($invoice);
+                }
+            }
+        }
+
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Invoice.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InvoiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InvoiceRepository::class)]
+class Invoice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $amount = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $sendAt = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    #[ORM\ManyToOne(inversedBy: 'invoices')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Customer $customer = null;
+
+    #[ORM\Column]
+    private ?int $chrono = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getSendAt(): ?\DateTimeImmutable
+    {
+        return $this->sendAt;
+    }
+
+    public function setSendAt(\DateTimeImmutable $sendAt): static
+    {
+        $this->sendAt = $sendAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): static
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getChrono(): ?int
+    {
+        return $this->chrono;
+    }
+
+    public function setChrono(int $chrono): static
+    {
+        $this->chrono = $chrono;
+
+        return $this;
+    }
+}
